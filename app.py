f54c1886b5fa82811a92417024a5bc7e1dcc6ba6
Faisal Rasbihan <faisalrasbihan@gmail.com>
2023-11-05 18:16:15 +0700

    change file name for app.py

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
 import streamlit as st
+from langchain.llms import OpenAI
+st.set_page_config(page_title="🦜🔗 DSW Product Finder App")
+st.title('🔍 DSW 2023 Product Finder App')
+st.caption("🚀 Product finder application made by Bandamology Team for the Data Science Weekend 2023 Data Hackathon")
 
-with st.sidebar:
-    st.write("**Streamlit app created by:**")
+# openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-    st.write("Bandarmology Team")
-    st.write("Faisal Rasbihan")
-    st.write("Taufik Eko Hidayanto")
-    st.write("Arda Putra Ryandika")
-    st.write("**Questions and answers sourced from:**")
-    st.caption(" 82 Product Owner Interview Questions to Avoid Hiring Imposters")
-    st.caption("By Stefan Wolpers | Version 8.01 | 2022-01-17")
-    st.caption("https://berlin-product-people.com/")
-    st.caption(
-        "Download link: https://age-of-product.com/42-scrum-product-owner-interview-questions/"
-    )
-    st.write("Copyright notice:")
-    st.caption(
-        "No part of this publication or its text may be made publicly available or, excepting personal use, reproduced, or distributed or translated into other languages without the prior written permission of Berlin Product People GmbH. If you would like permission to reproduce or otherwise publish any part or all of this publication or its text, including translations thereof, write to us at info@berlin-product-people.com addressed “Attention: Permissions Request.”"
-    )
-    st.caption("Materials in the app used with permission of Stefan Wolpers")
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+# def generate_response(input_text):
+#   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+#   st.info(llm(input_text))
+  
+def search_product(product_name):
+  ## Logic here
+  product_list = ['Roundup', 'Glyphosate', 'Fujiwan']
+  return product_list
 
+with st.sidebar.header("About"):
+  st.sidebar.markdown((
+      "[Bandar](https://bandar.streamlit.app) is a Product Finder Application for the submission of the 2023 Data Science Weekend Competition, Senior Professional Category."
+  ))
+with st.sidebar.header("Copyright notice"):
+  st.sidebar.markdown("No part of this publication or its text may be made publicly available or, excepting personal use, reproduced, or distributed or translated into other languages without the prior written permission of Berlin Product People GmbH. If you would like permission to reproduce or otherwise publish any part or all of this publication or its text, including translations thereof, write to us at info@berlin-product-people.com addressed “Attention: Permissions Request.”")
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+with st.form('my_form'):
+  text = st.text_input("Search product name", value="")
+  submitted = st.form_submit_button('Submit')
+  
+  # Submit button is pressed
+  if submitted:
+    products = search_product(text)
+    st.caption("Below are the possible product names")
+    for product in products:
+      with st.expander(product):
+        st.write("Product Type :")
 
-
-st.title("💬 Bandar Product Name Finder")
-text_search = st.text_input("Search in titles, questions and answers", value="")
-
-text = st.text_area(
-    # Instructions
-    "Product name not found, is this the product that you meant?",
-    # 'sample' variable that contains our keyphrases.
-    height=200,
-
-)
+  # # Delete later
+  # if not openai_api_key.startswith('sk-'):
+  #   st.warning('Please enter your OpenAI API key!', icon='⚠')
+  # if submitted and openai_api_key.startswith('sk-'):
+  #   generate_response(text)
+  # # Put inside search product
+  # if text == 'keong':
+  #   st.info('Product name not found, is this the product that you are referring to?')
